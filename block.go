be004a9e3546e2c179b5a302578d0cb2f2ff41cf
lcy1317 <213183464.seu.edu.cn>
2022-03-02 16:51:50 +0800

    First Commit - 基本原型

---
+++ block.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// 简化的区块链数据结构
+
+type Block struct {
+	Version       string // 版本号
+	Timestamp     int64  // 时间戳
+	Data          []byte // 区块存储的实际有效信息，也就是交易
+	PrevBlockHash []byte // 前一个块的哈希
+	Hash          []byte // 当前块的哈希
+	Nonce         int    // 随机值
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers) // 计算哈希
+
+	b.Hash = hash[:]
+}
+
+// 新建一个块
+
+func NewBlock(version string, data string, prevBlockHash []byte, nonce int) *Block {
+	block := &Block{version, time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, nonce}
+	block.SetHash()
+	return block
+}
---
+++ blockchain.go
@@ -0,0 +1,21 @@
+package main
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+// 创建一个带有创始区块的区块链。
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("0.0-beta0", "Genesis Block", []byte{}, 0)
+}
+
+// TODO: Version和Nonce的值现在是随便继承了之前的。
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(prevBlock.Version, data, prevBlock.Hash, prevBlock.Nonce)
+	bc.blocks = append(bc.blocks, newBlock)
+}
